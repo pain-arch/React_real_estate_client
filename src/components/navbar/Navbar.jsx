5f054a6e60b36e91f0b12d369451186cea93f864
pain-arch <himulol123@gmail.com>
2024-04-16 17:29:00 +0600

    navbar component Design with logo and navigation links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,8 +3,20 @@ import "./navbar.scss";
 function Navbar() {
     return (
         <nav className="navbar">
-            <div className="lest">Left</div>
-            <div className="right">Right</div>
+            <div className="left">
+                <a href="/" className="logo">
+                    <img src="/logo.png" alt="Logo" />
+                    <span>Real Estate</span>
+                </a>
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+                <a href="/services">Agents</a>
+                <a href="/contact">Contact</a>
+            </div>
+            <div className="right">
+                <a href="/login" className="login_btn">Login</a>
+                <a href="/register" className="register_btn">Sign Up</a>
+            </div>
         </nav>
     )
 }
