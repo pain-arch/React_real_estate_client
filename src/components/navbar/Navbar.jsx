568771b8a657d39d2261d106c61712b898d6ef62
pain-arch <himulol123@gmail.com>
2024-04-16 17:56:24 +0600

    Adding responsive Hamburger  menu to navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -16,6 +16,17 @@ function Navbar() {
             <div className="right">
                 <a href="/login" className="login_btn">Login</a>
                 <a href="/register" className="register_btn">Sign Up</a>
+                <div className="menuIcon">
+                    <img src="/menu.png" alt="Menu" />
+                </div>
+                <div className="menu">
+                    <a href="/">Home</a>
+                    <a href="/about">About</a>
+                    <a href="/services">Agents</a>
+                    <a href="/contact">Contact</a>
+                    <a href="/login">Login</a>
+                    <a href="/register">Sign Up</a>
+                </div>
             </div>
         </nav>
     )
