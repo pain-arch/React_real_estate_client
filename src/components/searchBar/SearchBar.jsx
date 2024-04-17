60050ee940a5802425528b2b0a9fbac4f4b7cfad
pain-arch <himulol123@gmail.com>
2024-04-18 00:17:43 +0600

    basic html design of SearchBar.jsx

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,9 +1,34 @@
-import './searchBar.scss'
+import "./searchBar.scss";
 
-function SearchBar(){
+function SearchBar() {
   return (
-    <div className='searchBar'>SearchBar</div>
-  )
+    <div className="searchBar">
+      <div className="type">
+        <button>Buy</button>
+        <button>Rent</button>
+      </div>
+      <form>
+        <input type="text" name="location" placeholder="City Location" />
+        <input
+          type="number"
+          name="minPrice"
+          min={0}
+          max={10000000}
+          placeholder="Min Price"
+        />
+        <input
+          type="number"
+          name="maxPrice"
+          min={0}
+          max={10000000}
+          placeholder="Max Price"
+        />
+        <button>
+          <img src="/search.png" alt="search" />
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default SearchBar
+export default SearchBar;
