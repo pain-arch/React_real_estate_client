ed55636b98bdff10ed1336e0367f1fe3cb2a9bc7
pain-arch <himulol123@gmail.com>
2024-04-17 14:35:02 +0600

    search bar and box section

--- src/routes/homePage/homePage.jsx
+++ src/routes/homePage/homePage.jsx
@@ -1,14 +1,44 @@
+import SearchBar from "../../components/searchBar/SearchBar";
 import "./homePage.scss";
 
 function HomePage() {
-    return (
-        <div className="homePage">
-            <div className="textContainer">Text</div>
-            <div className="imgContainer">
-                <img src="/bg.png" alt="hero_bg" />
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">
+            Find Real Estate Properties That Are Right For You
+          </h1>
+          <p className="description">
+            Are you embarking on the exciting journey of finding your dream home
+            or investment property? With the vast array of options available,
+            its essential to navigate the real estate market with confidence
+            and clarity.
+          </p>
+          {/* searchbar component */}
+          <SearchBar />
+          {/* box part */}
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
             </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained </h2>
+            </div>
+            <div className="box">
+              <h1>1200+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
         </div>
-    );    
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="hero_bg" />
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
