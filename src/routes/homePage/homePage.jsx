59ba6ff1a36b6fc5cc49c7a08934fb89ba815a9f
pain-arch <himulol123@gmail.com>
2024-04-17 14:06:35 +0600

    Updating homePage component with image container

--- src/routes/homePage/homePage.jsx
+++ src/routes/homePage/homePage.jsx
@@ -1,9 +1,14 @@
-import './homePage.scss'
+import "./homePage.scss";
 
-function HomePage(){
-  return (
-    <div className='homePage'>HomePage</div>
-  )
+function HomePage() {
+    return (
+        <div className="homePage">
+            <div className="textContainer">Text</div>
+            <div className="imgContainer">
+                <img src="/bg.png" alt="hero_bg" />
+            </div>
+        </div>
+    );    
 }
 
-export default HomePage
+export default HomePage;
