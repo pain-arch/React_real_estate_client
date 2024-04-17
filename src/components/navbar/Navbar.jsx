3b07767198518f716273fb78ff663a8fabdaef85
pain-arch <himulol123@gmail.com>
2024-04-17 12:43:26 +0600

    Update navbar component with responsive Hamburger menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,35 +1,46 @@
+import { useState } from "react";
 import "./navbar.scss";
 
 function Navbar() {
-    return (
-        <nav className="navbar">
-            <div className="left">
-                <a href="/" className="logo">
-                    <img src="/logo.png" alt="Logo" />
-                    <span>Real Estate</span>
-                </a>
-                <a href="/">Home</a>
-                <a href="/about">About</a>
-                <a href="/services">Agents</a>
-                <a href="/contact">Contact</a>
-            </div>
-            <div className="right">
-                <a href="/login" className="login_btn">Login</a>
-                <a href="/register" className="register_btn">Sign Up</a>
-                <div className="menuIcon">
-                    <img src="/menu.png" alt="Menu" />
-                </div>
-                <div className="menu">
-                    <a href="/">Home</a>
-                    <a href="/about">About</a>
-                    <a href="/services">Agents</a>
-                    <a href="/contact">Contact</a>
-                    <a href="/login">Login</a>
-                    <a href="/register">Sign Up</a>
-                </div>
-            </div>
-        </nav>
-    )
+  const [open, setOpen] = useState(false);
+
+  return (
+    <nav className="navbar">
+      <div className="left">
+        <a href="/" className="logo">
+          <img src="/logo.png" alt="Logo" />
+          <span>Real Estate</span>
+        </a>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/services">Agents</a>
+        <a href="/contact">Contact</a>
+      </div>
+      <div className="right">
+        <a href="/login" className="login_btn">
+          Login
+        </a>
+        <a href="/register" className="register_btn">
+          Sign Up
+        </a>
+        <div className="menuIcon">
+          <img
+            src="/menu.png"
+            alt="Menu"
+            onClick={() => setOpen((prev) => !prev)}
+          />
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+          <a href="/services">Agents</a>
+          <a href="/contact">Contact</a>
+          <a href="/login">Login</a>
+          <a href="/register">Sign Up</a>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
