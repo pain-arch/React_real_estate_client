113da8c24998c991d80dcb3a9fa195064c9f7a47
pain-arch <himulol123@gmail.com>
2024-04-18 19:56:04 +0600

    succesfullly completed routing system

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
-import Navbar from "./components/navbar/Navbar";
-import "./layout.scss";
 import HomePage from "./routes/homePage/homePage";
+import Layout from "./routes/layout/layout";
+import ListPage from "./routes/listPage/listPage";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/list", element: <ListPage /> },
+    ],
+  },
+]);
+
 
 function App() {
   return (
-    <div className="layout">
-      <div className="navbar">
-        <Navbar />
-      </div>
-      <div className="content">
-        <HomePage />
-      </div>
-    </div>
+    // <div className="layout">
+    //   <div className="navbar">
+    //     <Navbar />
+    //   </div>
+    //   <div className="content">
+    //     <HomePage />
+    //   </div>
+    // </div>
+    <RouterProvider router={router}/>
   );
 }
 
