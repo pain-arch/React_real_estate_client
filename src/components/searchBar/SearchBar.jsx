fc26d7f5148be365ce42e3d892c2a8dc990a4c32
pain-arch <himulol123@gmail.com>
2024-04-18 13:43:40 +0600

    searchbar button funtionality

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,11 +1,30 @@
+import { useState } from "react";
 import "./searchBar.scss";
 
+const types = ["buy", "rent"];
+
 function SearchBar() {
+  const [query, setQuery] = useState({
+    type: "buy",
+    location: "",
+    minPrice: 0,
+    maxPrice: 0,
+  });
+
+  const switchType = (value) => {
+    setQuery((prev) => ({ ...prev, type: value }))
+  };
+
   return (
     <div className="searchBar">
       <div className="type">
-        <button>Buy</button>
-        <button>Rent</button>
+        {types.map((type) => (
+          <button key={type} onClick={() => switchType(type)}
+          className={query.type === type ? "active" : ""}
+          >
+            {type}
+          </button>
+        ))}
       </div>
       <form>
         <input type="text" name="location" placeholder="City Location" />
