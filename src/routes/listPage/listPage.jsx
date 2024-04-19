75ec3753d64a50eb643ad4cb6c870726755c7f87
pain-arch <himulol123@gmail.com>
2024-04-19 16:22:20 +0600

    Create Card and Filter components  and add them to listPage.jsx

--- src/routes/listPage/listPage.jsx
+++ src/routes/listPage/listPage.jsx
@@ -1,3 +1,5 @@
+import Card from "../../components/card/card";
+import Filter from "../../components/filter/filter";
 import { listData } from "../../lib/dummyData";
 import "./listPage.scss";
 
@@ -8,7 +10,10 @@ function ListPage() {
     <div className="listPage">
       <div className="listContainer">
         <div className="wrapper">
-          
+          <Filter />
+          {data.map(item => (
+              <Card key={item.id} data={item} />
+            ))}
         </div>
       </div>
       <div className="mapContainer">map</div>
