8ceded19f7926ca0facdbbf985769ee1d1808814
pain-arch <himulol123@gmail.com>
2024-04-19 16:12:49 +0600

    Refactor listPage component and styles

--- src/routes/listPage/listPage.jsx
+++ src/routes/listPage/listPage.jsx
@@ -1,9 +1,19 @@
-import './listPage.scss'
+import { listData } from "../../lib/dummyData";
+import "./listPage.scss";
 
-function ListPage(){
+function ListPage() {
+  const data = listData;
+
   return (
-    <div className='listPage'>ListPage</div>
-  )
+    <div className="listPage">
+      <div className="listContainer">
+        <div className="wrapper">
+          
+        </div>
+      </div>
+      <div className="mapContainer">map</div>
+    </div>
+  );
 }
 
-export default ListPage
+export default ListPage;
