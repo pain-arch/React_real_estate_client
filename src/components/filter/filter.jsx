046688c0c55e6a5ba7128ea91043ecb48afcb857
pain-arch <himulol123@gmail.com>
2024-04-19 17:45:26 +0600

    Refactor filter component and add search functionality

--- src/components/filter/filter.jsx
+++ src/components/filter/filter.jsx
@@ -1,11 +1,78 @@
-import './filter.scss'
+import "./filter.scss";
 
-function Filter(){
+function Filter() {
   return (
-    <div className='filter'>
+    <div className="filter">
+      <h1>
+        Search results for <b>London</b>
+      </h1>
+      {/* top part       */}
+      <div className="top">
+        <div className="item">
+          <label htmlFor="city">Location</label>
+          <input
+            type="text"
+            id="city"
+            name="city"
+            placeholder="City, State, Country"
+          />
+        </div>
+      </div>
           
+
+      {/* bottom part */}
+      <div className="bottom">
+        <div className="item">
+            <label htmlFor="type">Type</label>
+            <select name="type" id="type">
+                <option value="all">All</option>
+                <option value="buy">Buy</option>
+                <option value="rent">Rent</option>
+            </select>
+        </div>
+        <div className="item">
+          <label htmlFor="property">Property</label>
+          <select name="property" id="property">
+                <option value="all">All</option>      
+                <option value="apertment">Apertment</option>
+                <option value="house">House</option>
+                <option value="condo">Condo</option>
+                <option value="land">Land</option>
+          </select>
+        </div>
+        <div className="item">
+          <label htmlFor="minPrice">Min Price</label>
+          <input
+            type="number"
+            id="minPrice"
+            name="minPrice"
+            placeholder="any"
+          />
+        </div>
+        <div className="item">
+          <label htmlFor="maxPrice">Max Price</label>
+          <input
+            type="number"
+            id="maxPrice"
+            name="maxPrice"
+            placeholder="any"
+          />
+        </div>
+        <div className="item">
+          <label htmlFor="bedroom">Bedroom</label>
+          <input
+            type="number"
+            id="bedroom"
+            name="bedroom"
+            placeholder="any"
+          />
+        </div>
+        <button>
+          <img src="/search.png" alt="search" />
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Filter
+export default Filter;
