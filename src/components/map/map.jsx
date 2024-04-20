685e45c394184be34951eadbdf5fcda8d29e09be
RfxNaushad~ <rfx_naushad@outlook.com>
2024-04-20 12:44:04 +0600

    pin changes done

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -1,19 +1,30 @@
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
 import "./map.scss";
 import "leaflet/dist/leaflet.css";
+import Pin from "../pin/pin";
 
-function Map() {
+function Map({ item }) {
+
+  console.log(item);
+
   return (
-    <MapContainer center={[23.746466, 90.376015]} zoom={7} scrollWheelZoom={false} className="map">
+    <MapContainer
+      center={[51.5074, -0.1278]}
+      zoom={7}
+      scrollWheelZoom={false}
+      className="map"
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[23.746466, 90.376015]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+
+    {
+            item?.map((it) => (
+              <Pin key={it.id} item={it} />
+            ))      
+    }
+      
     </MapContainer>
   );
 }
