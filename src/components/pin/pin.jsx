0619c09408e717a54b997892e6aa105d977795f4
pain-arch <himulol123@gmail.com>
2024-04-20 12:26:26 +0600

    pin update

---
+++ src/components/pin/pin.jsx
@@ -0,0 +1,32 @@
+import { MapContainer, TileLayer } from "react-leaflet";
+import "./map.scss";
+import "leaflet/dist/leaflet.css";
+import Pin from "../pin/pin";
+
+function Map({ item }) {
+
+  console.log(item);
+
+  return (
+    <MapContainer
+      center={[51.5074, -0.1278]}
+      zoom={7}
+      scrollWheelZoom={false}
+      className="map"
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+    {
+            item?.map((i) => (
+              <Pin key={i.id} data={it} />
+            ))      
+    }
+      
+    </MapContainer>
+  );
+}
+
+export default Map;
