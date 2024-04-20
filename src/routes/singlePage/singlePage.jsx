107d26ac4ab57f435658ddcaca612c6455f496fa
pain-arch <himulol123@gmail.com>
2024-04-20 16:26:06 +0600

    Add Slider component to SinglePage

--- src/routes/singlePage/singlePage.jsx
+++ src/routes/singlePage/singlePage.jsx
@@ -1,9 +1,25 @@
-import './singlePage.scss'
+import Slider from "../../components/slider/slider";
+import "./singlePage.scss";
 
-function SinglePage(){
+function SinglePage() {
   return (
-    <div className='singlePage'>SinglePage</div>
-  )
+    <div className="singlePage">
+      <div className="details">
+        <div className="wrapper">
+          <Slider />
+          <div className="info">
+            <div className="top"></div>
+            <div className="bottom"></div>
+          </div>
+        </div>
+      </div>
+      <div className="features">
+        <div className="wrapper">
+          feat
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default SinglePage
+export default SinglePage;
