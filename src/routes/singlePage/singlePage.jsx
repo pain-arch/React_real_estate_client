411b96b9575f25d1918cc6e7f7153cbd0bfbab15
pain-arch <himulol123@gmail.com>
2024-04-22 13:07:46 +0600

    Refactor: SinglePage component to include user data and improve layout

--- src/routes/singlePage/singlePage.jsx
+++ src/routes/singlePage/singlePage.jsx
@@ -1,15 +1,31 @@
 import Slider from "../../components/slider/slider";
 import "./singlePage.scss";
+import {singlePostData, userData} from "../../lib/dummyData.js";
 
 function SinglePage() {
   return (
     <div className="singlePage">
       <div className="details">
         <div className="wrapper">
-          <Slider />
+          <Slider images={singlePostData.images} />
           <div className="info">
-            <div className="top"> top</div>
-            <div className="bottom"></div>
+            <div className="top">
+              <div className="post">
+                <h1>{singlePostData.title}</h1>
+                <div className="address">
+                   <img src="/pin.png" alt="pin_icon" />
+                   <span>{ singlePostData.address }</span>
+                </div>
+                <div className="price">$ { singlePostData.price }</div>
+              </div>
+              <div className="user">
+                <img src={ userData.img } alt="user" />
+                <span>{ userData.name }</span>
+              </div>
+            </div>
+            <div className="bottom">
+              {singlePostData.description}
+            </div>
           </div>
         </div>
       </div>
