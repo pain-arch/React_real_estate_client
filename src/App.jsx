1eac2623b570bf45727e3899660086edf2daa4ee
pain-arch <himulol123@gmail.com>
2024-04-24 13:07:30 +0600

    Add ProfilePage component to router configuration in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import HomePage from "./routes/homePage/homePage";
 import Layout from "./routes/layout/layout";
 import ListPage from "./routes/listPage/listPage";
+import ProfilePage from "./routes/profilePage/profilePage";
 import SinglePage from "./routes/singlePage/singlePage";
 import {
   createBrowserRouter,
@@ -15,7 +16,8 @@ const router = createBrowserRouter([
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/list", element: <ListPage /> },
-      { path: "/:id", element: <SinglePage />}
+      { path: "/:id", element: <SinglePage /> },
+      { path: "/profile", element: <ProfilePage />}
     ],
   },
 ]);
